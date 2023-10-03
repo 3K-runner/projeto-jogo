15689c7099f92022a011499d1a75b5b6451cf6bc
3K-runner <144951500+3K-runner@users.noreply.github.com>
2023-10-02 23:53:19 -0300

    Sprite do pássaro 2 no web.js

--- web.js
+++ web.js
@@ -4,6 +4,69 @@ const ctx = canvas.getContext('2d')
 // Constant that represents the art grid size
 const BITS = 32
 
+// Art "Sprites"
+const SGUINE = [{x: 7, y: 2, l: 2, colour: 'rgb(56,56,56)'},
+{x: 7, y: 3, l: 2, colour: 'rgb(56,56,56)'},
+
+{x: 5, y: 4, l: 6, colour: 'rgb(0,0,0)'},
+
+{x: 4, y: 5, l: 8, colour: 'rgb(0,0,0)'},
+{x: 5, y: 5, l: 6, colour: 'rgb(165,20,17)'},
+
+{x: 3, y: 6, l:10, colour: 'rgb(0,0,0)'},
+{x: 4, y: 6, l: 8, colour: 'rgb(165,20,17)'}, 
+{x: 5, y: 6, l: 2, colour: 'rgb(255,255,255)'},
+{x: 9, y: 6, l: 2, colour: 'rgb(255,255,255)'},
+ 
+{x: 2, y: 7, l:12, colour: 'rgb(0,0,0)'},
+{x: 4, y: 7, l: 8, colour: 'rgb(165,20,17)'},
+{x: 5, y: 7, l: 6, colour: 'rgb(255,255,255)'}, 
+{x: 6, y: 7, l: 4, colour: 'rgb(0,0,0)'},
+{x: 7, y: 7, l: 2, colour: 'rgb(214,173,34)'},
+
+{x: 1, y: 8, l:14, colour: 'rgb(0,0,0)'},
+{x: 2, y: 8, l:12, colour: 'rgb(56,56,56)'},
+{x: 3, y: 8, l:10, colour: 'rgb(0,0,0)'}, 
+{x: 4, y: 8, l: 8, colour: 'rgb(55,57,191)'},
+{x: 5, y: 8, l: 6, colour: 'rgb(165,20,17)'},
+{x: 7, y: 8, l: 2, colour: 'rgb(214,173,34)'},
+
+{x: 1, y: 9, l:14, colour: 'rgb(0,0,0)'},
+{x: 2, y: 9, l:12, colour: 'rgb(154,154,154)'},
+{x: 3, y: 9, l:10, colour: 'rgb(0,0,0)'}, 
+{x: 4, y: 9, l: 8, colour: 'rgb(154,154,154)'},
+{x: 5, y: 9, l: 6, colour: 'rgb(165,20,17)'},
+{x: 6, y: 9, l: 4, colour: 'rgb(56,56,56)'},
+{x: 7, y: 9, l: 2, colour: 'rgb(55,57,191)'},
+
+{x: 1, y:10, l:14, colour: 'rgb(0,0,0)'},
+{x: 2, y:10, l:12, colour: 'rgb(56,56,56)'},
+{x: 3, y:10, l:10, colour: 'rgb(0,0,0)'}, 
+{x: 4, y:10, l: 8, colour: 'rgb(56,56,56)'},
+{x: 5, y:10, l: 6, colour: 'rgb(55,57,191)'},
+{x: 6, y:10, l: 4, colour: 'rgb(56,56,56)'},
+
+{x: 2, y:11, l:12, colour: 'rgb(0,0,0)'},
+{x: 4, y:11, l: 8, colour: 'rgb(154,154,154)'},
+{x: 5, y:11, l: 6, colour: 'rgb(56,56,56)'}, 
+{x: 6, y:11, l: 4, colour: 'rgb(154,154,154)'},
+{x: 7, y:11, l: 2, colour: 'rgb(56,56,56)'},
+
+{x: 4, y:12, l: 8, colour: 'rgb(0,0,0)'}, 
+{x: 5, y:12, l: 6, colour: 'rgb(154,154,154)'},
+{x: 6, y:12, l: 4, colour: 'rgb(0,0,0)'},
+
+{x: 5, y:13, l: 1, colour: 'rgb(0,0,0)'}, 
+{x:10, y:13, l: 1, colour: 'rgb(0,0,0)'},
+
+{x: 5, y:14, l: 1, colour: 'rgb(0,0,0)'},
+{x:10, y:14, l: 1, colour: 'rgb(0,0,0)'}, 
+
+{x: 4, y:15, l: 1, colour: 'rgb(0,0,0)'},
+{x: 6, y:15, l: 1, colour: 'rgb(0,0,0)'},
+{x: 9, y:15, l: 1, colour: 'rgb(0,0,0)'},
+{x:11, y:15, l: 1, colour: 'rgb(0,0,0)'}]
+
 // Mutable state
 let state = initialState()
 
@@ -34,10 +97,15 @@ const draw = () => {
   // draw Ghosts
   ctx.fillStyle = 'rgb(255,182,193)'
   ctx.fillRect(x(state.ghosts[0].x), y(state.ghosts[0].y), x(1), y(1))
+  
   ctx.fillStyle = 'rgb(255,0,0)'
   ctx.fillRect(x(state.ghosts[1].x), y(state.ghosts[1].y), x(1), y(1))
-  ctx.fillStyle = 'rgb(0,255,255)'
-  ctx.fillRect(x(state.ghosts[2].x), y(state.ghosts[2].y), x(1), y(1))
+
+  SGUINE.map(p => {
+  ctx.fillStyle = p.colour
+  ctx.fillRect(xg(state.ghosts[2])(p), yg(state.ghosts[2])(p), x(p.l/BITS), y(1/BITS))
+  })
+
   ctx.fillStyle = 'rgb(236,236,83)'
   ctx.fillRect(x(state.ghosts[3].x), y(state.ghosts[3].y), x(1), y(1))
 
