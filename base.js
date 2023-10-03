b87da539839c43f30657c394220d657098f4dc2d
3K-runner <144951500+3K-runner@users.noreply.github.com>
2023-10-03 00:57:29 -0300

    Atualizar distance e orderMoves

--- base.js
+++ base.js
@@ -3,27 +3,15 @@ const merge     = o1 => o2 => Object.assign({}, o1, o2)
 const rnd       = min => max => Math.floor(Math.random() * max) + min
 const mod       = x => y => y < 0 ? (x - 1) : (y > (x - 1) ? 0 : y) 
 //Calculates the distance between two given dots
-const distance  = (p1) =>  (p2) => Math.trunc(Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2))
+const distance  = (p1) =>  (p2) => Math.trunc((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
 // Finds the smallest distance in relation to the ghosts
 const orderMoves = (moves) => (ghostPosition) => (snakePosition) => {
   
   // Possible directions
-  const distances = moves.map((move) => {
-    switch (move) {
-      case "NORTH":
-        return distance(ghostPosition)({ x: snakePosition.x, y: snakePosition.y - 1 });
-      case "SOUTH":
-        return distance(ghostPosition)({ x: snakePosition.x, y: snakePosition.y + 1 });
-      case "WEST":
-        return distance(ghostPosition)({ x: snakePosition.x + 1, y: snakePosition.y });
-      case "EAST":
-        return distance(ghostPosition)({ x: snakePosition.x - 1, y: snakePosition.y });
-      default:
-    }
-  })
+  const distances = moves.map((move) => distance(ghostPosition)({ x: snakePosition.x + move.x, y: snakePosition.y + move.y}))
 
   // Ordered directions
- const movementInOrder = [...moves].sort((a, b) => distances[moves.indexOf(a)] - distances[moves.indexOf(b)])
+ const movementInOrder = [...moves].toSorted((a, b) => distances[moves.indexOf(a)] - distances[moves.indexOf(b)])
 
   return movementInOrder;
 }
