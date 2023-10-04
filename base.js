dd88f7f22752f70fd3646648c8024b017e0ec351
Caio <118756888+caiomagn0@users.noreply.github.com>
2023-10-04 19:43:52 -0300

    Update base.js

--- base.js
+++ base.js
@@ -4,11 +4,11 @@ const rnd       = min => max => Math.floor(Math.random() * max) + min
 const mod       = x => y => y < 0 ? (x - 1) : (y > (x - 1) ? 0 : y) 
 //Calculates the distance between two given dots
 const distance  = (p1) =>  (p2) => Math.trunc((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
-// Finds the smallest distance in relation to the ghosts
-const orderMoves = (moves) => (ghostPosition) => (snakePosition) => {
+// Finds the smallest distance in relation to the birds
+const orderMoves = (moves) => (birdPosition) => (snakePosition) => {
   
   // Possible directions
-  const distances = moves.map((move) => distance(ghostPosition)({ x: snakePosition.x + move.x, y: snakePosition.y + move.y}))
+  const distances = moves.map((move) => distance(birdPosition)({ x: snakePosition.x + move.x, y: snakePosition.y + move.y}))
 
   // Ordered directions
  const movementInOrder = [...moves].sort((a, b) => distances[moves.indexOf(a)] - distances[moves.indexOf(b)])
