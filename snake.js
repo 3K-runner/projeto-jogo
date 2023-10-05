13bffb28314a11f9c195becdfd2c24bc63465b4b
3K-runner <144951500+3K-runner@users.noreply.github.com>
2023-10-05 10:09:06 -0300

    Atualização COLS e ROWS

--- snake.js
+++ snake.js
@@ -2,6 +2,9 @@ const base = require('./base')
 Object.getOwnPropertyNames(base).map(p => global[p] = base[p])
 
 // Constants 
+// maze size
+const COLS  = 20
+const ROWS  = 14
 // movement 
 const NORTH = { x: 0, y:-1 } 
 const SOUTH = { x: 0, y: 1 } 
@@ -9,159 +12,89 @@ const EAST  = { x: 1, y: 0 }
 const WEST  = { x:-1, y: 0 }
 const STOP  = { x: 0, y: 0 } // Move Stop
 // position
-const WALLS  = [{ x: 0, y: 0 },
-                { x: 1, y: 0 },
-                { x: 2, y: 0 },
-                { x: 3, y: 0 },
-                { x: 4, y: 0 },
-                { x: 5, y: 0 },
-                { x: 6, y: 0 },
-                { x: 7, y: 0 },
-                { x: 8, y: 0 },
-                { x: 9, y: 0 },
-                { x:10, y: 0 },
-                { x:11, y: 0 },
-                { x:12, y: 0 },
-                { x:13, y: 0 },
-                { x:14, y: 0 },
-                { x:15, y: 0 },
-                { x:16, y: 0 },
-                { x:17, y: 0 },
-                { x:18, y: 0 },
-                { x:19, y: 0 },
+const WALLS  = [{ x: 0, y: 0 }, { x: 1, y: 0 },
+                { x: 2, y: 0 }, { x: 3, y: 0 },
+                { x: 4, y: 0 }, { x: 5, y: 0 },
+                { x: 6, y: 0 }, { x: 7, y: 0 },
+                { x: 8, y: 0 }, { x: 9, y: 0 },
+                { x:10, y: 0 }, { x:11, y: 0 },
+                { x:12, y: 0 }, { x:13, y: 0 },
+                { x:14, y: 0 }, { x:15, y: 0 },
+                { x:16, y: 0 }, { x:17, y: 0 },
+                { x:18, y: 0 }, { x:19, y: 0 },
                 
-                { x: 0, y: 1 },
-                { x:19, y: 1 },
+                { x: 0, y: 1 }, { x:19, y: 1 },
                 
-                { x: 0, y: 2 },
-                { x: 2, y: 2 },
-                { x: 3, y: 2 },
-                { x: 5, y: 2 },
-                { x: 8, y: 2 },
-                { x: 9, y: 2 },
-                { x:10, y: 2 },
-                { x:11, y: 2 },
-                { x:14, y: 2 },
-                { x:16, y: 2 },
-                { x:17, y: 2 },
-                { x:19, y: 2 },
+                { x: 0, y: 2 }, { x: 2, y: 2 },
+                { x: 3, y: 2 }, { x: 5, y: 2 },
+                { x: 8, y: 2 }, { x: 9, y: 2 },
+                { x:10, y: 2 }, { x:11, y: 2 },
+                { x:14, y: 2 }, { x:16, y: 2 },
+                { x:17, y: 2 }, { x:19, y: 2 },
                 
-                { x: 0, y: 3 },
-                { x: 2, y: 3 },
-                { x: 3, y: 3 },
-                { x: 5, y: 3 },
-                { x: 6, y: 3 },
-                { x: 9, y: 3 },
-                { x:10, y: 3 },
-                { x:13, y: 3 },
-                { x:14, y: 3 },
-                { x:16, y: 3 },
-                { x:17, y: 3 },
-                { x:19, y: 3 },
+                { x: 0, y: 3 }, { x: 2, y: 3 },
+                { x: 3, y: 3 }, { x: 5, y: 3 },
+                { x: 6, y: 3 }, { x: 9, y: 3 },
+                { x:10, y: 3 }, { x:13, y: 3 },
+                { x:14, y: 3 }, { x:16, y: 3 },
+                { x:17, y: 3 }, { x:19, y: 3 },
                  
-                { x: 0, y: 4 },
-                { x: 5, y: 4 },
-                { x:14, y: 4 },
-                { x:19, y: 4 },
+                { x: 0, y: 4 }, { x: 5, y: 4 },
+                { x:14, y: 4 }, { x:19, y: 4 },
                 
-                { x: 0, y: 5 },
-                { x: 1, y: 5 },
-                { x: 2, y: 5 },
-                { x: 3, y: 5 },
-                { x: 5, y: 5 },
-                { x: 7, y: 5 },
-                { x: 8, y: 5 },
-                { x:11, y: 5 },
-                { x:12, y: 5 },
-                { x:14, y: 5 },
-                { x:16, y: 5 },
-                { x:17, y: 5 },
-                { x:18, y: 5 },
-                { x:19, y: 5 },
+                { x: 0, y: 5 }, { x: 1, y: 5 },
+                { x: 2, y: 5 }, { x: 3, y: 5 },
+                { x: 5, y: 5 }, { x: 7, y: 5 },
+                { x: 8, y: 5 }, { x:11, y: 5 },
+                { x:12, y: 5 }, { x:14, y: 5 },
+                { x:16, y: 5 }, { x:17, y: 5 },
+                { x:18, y: 5 }, { x:19, y: 5 },
                  
-                { x: 7, y: 6 },
-                { x:12, y: 6 },
+                { x: 7, y: 6 }, { x:12, y: 6 },
                 
-                { x: 0, y: 7 },
-                { x: 2, y: 7 },
-                { x: 3, y: 7 },
-                { x: 5, y: 7 },
-                { x: 7, y: 7 },
-                { x: 8, y: 7 },
-                { x: 9, y: 7 },
-                { x:10, y: 7 },
-                { x:11, y: 7 },
-                { x:12, y: 7 },
-                { x:14, y: 7 },
-                { x:16, y: 7 },
-                { x:17, y: 7 },
-                { x:19, y: 7 },
+                { x: 0, y: 7 }, { x: 2, y: 7 },
+                { x: 3, y: 7 }, { x: 5, y: 7 },
+                { x: 7, y: 7 }, { x: 8, y: 7 },
+                { x: 9, y: 7 }, { x:10, y: 7 },
+                { x:11, y: 7 }, { x:12, y: 7 },
+                { x:14, y: 7 }, { x:16, y: 7 },
+                { x:17, y: 7 }, { x:19, y: 7 },
                 
-                { x: 0, y: 8 },
-                { x:19, y: 8 },
+                { x: 0, y: 8 }, { x:19, y: 8 },
                 
-                { x: 0, y: 9 },
-                { x: 1, y: 9 },
-                { x: 3, y: 9 },
-                { x: 5, y: 9 },
-                { x: 7, y: 9 },
-                { x: 8, y: 9 },
-                { x: 9, y: 9 },
-                { x:10, y: 9 },
-                { x:11, y: 9 },
-                { x:12, y: 9 },
-                { x:14, y: 9 },
-                { x:16, y: 9 },
-                { x:18, y: 9 },
-                { x:19, y: 9 },
+                { x: 0, y: 9 }, { x: 1, y: 9 },
+                { x: 3, y: 9 }, { x: 5, y: 9 },
+                { x: 7, y: 9 }, { x: 8, y: 9 },
+                { x: 9, y: 9 }, { x:10, y: 9 },
+                { x:11, y: 9 }, { x:12, y: 9 },
+                { x:14, y: 9 }, { x:16, y: 9 },
+                { x:18, y: 9 }, { x:19, y: 9 },
                 
-                { x: 0, y:10 },
-                { x: 5, y:10 },
-                { x: 9, y:10 },
-                { x:10, y:10 },
-                { x:14, y:10 },
-                { x:19, y:10 },
+                { x: 0, y:10 }, { x: 5, y:10 },
+                { x: 9, y:10 }, { x:10, y:10 },
+                { x:14, y:10 }, { x:19, y:10 },
                 
-                { x: 0, y:11 },
-                { x: 2, y:11 },
-                { x: 3, y:11 },
-                { x: 4, y:11 },
-                { x: 5, y:11 },
-                { x: 6, y:11 },
-                { x: 7, y:11 },
-                { x: 9, y:11 },
-                { x:10, y:11 },
-                { x:12, y:11 },
-                { x:13, y:11 },
-                { x:14, y:11 },
-                { x:15, y:11 },
-                { x:16, y:11 },
-                { x:17, y:11 },
-                { x:19, y:11 },
+                { x: 0, y:11 }, { x: 2, y:11 },
+                { x: 3, y:11 }, { x: 4, y:11 },
+                { x: 5, y:11 }, { x: 6, y:11 },
+                { x: 7, y:11 }, { x: 9, y:11 },
+                { x:10, y:11 }, { x:12, y:11 },
+                { x:13, y:11 }, { x:14, y:11 },
+                { x:15, y:11 }, { x:16, y:11 },
+                { x:17, y:11 }, { x:19, y:11 },
                 
-                { x: 0, y:12 },
-                { x:19, y:12 },
+                { x: 0, y:12 }, { x:19, y:12 },
                 
-                { x: 0, y:13 },
-                { x: 1, y:13 },
-                { x: 2, y:13 },
-                { x: 3, y:13 },
-                { x: 4, y:13 },
-                { x: 5, y:13 },
-                { x: 6, y:13 },
-                { x: 7, y:13 },
-                { x: 8, y:13 },
-                { x: 9, y:13 },
-                { x:10, y:13 },
-                { x:11, y:13 },
-                { x:12, y:13 },
-                { x:13, y:13 },
-                { x:14, y:13 },
-                { x:15, y:13 },
-                { x:16, y:13 },
-                { x:17, y:13 },
-                { x:18, y:13 },
-                { x:19, y:13 }]
+                { x: 0, y:13 }, { x: 1, y:13 },
+                { x: 2, y:13 }, { x: 3, y:13 },
+                { x: 4, y:13 }, { x: 5, y:13 },
+                { x: 6, y:13 }, { x: 7, y:13 },
+                { x: 8, y:13 }, { x: 9, y:13 },
+                { x:10, y:13 }, { x:11, y:13 },
+                { x:12, y:13 }, { x:13, y:13 },
+                { x:14, y:13 }, { x:15, y:13 },
+                { x:16, y:13 }, { x:17, y:13 },
+                { x:18, y:13 }, { x:19, y:13 }]
 const FRUITS = [{ x:16, y: 1}]
 const START  = { x: 9, y: 8 } // Starting position
 const STARTBIRDS = [{ x: 8, y: 6 }, 
@@ -266,13 +199,13 @@ const nextPecks = state => [nextPeck1(state),
 const nextApple = state => state.apple.filter(wontEat(state))
 
 const nextHead  = state => ({
-      x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
-      y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
+      x: mod(COLS)(state.snake[0].x + state.moves[0].x),
+      y: mod(ROWS)(state.snake[0].y + state.moves[0].y)
     })
 
 const nextBeak  = state => i => move => ({
-      x: mod(state.cols)(state.birds[i].x + move.x),
-      y: mod(state.rows)(state.birds[i].y + move.y)
+      x: mod(COLS)(state.birds[i].x + move.x),
+      y: mod(ROWS)(state.birds[i].y + move.y)
     })
 
 const nextSnake = state => willCrash(state)
@@ -296,14 +229,12 @@ const nextBirds = state => pointEq(state.moves[0])(STOP)
    
 // Randomness
 const rndPos = table => ({
-  x: rnd(0)(table.cols - 1),
-  y: rnd(0)(table.rows - 1)
+  x: rnd(0)(COLS - 1),
+  y: rnd(0)(ROWS - 1)
 })
 
 // Initial state
 const initialState = () => ({
-  cols:  20,
-  rows:  14,
   moves: [STOP], 
   snake: [START],
   apple: FRUITS,
@@ -313,8 +244,6 @@ const initialState = () => ({
 
 // Bird eats snake state
 const eatenState = state => ({
-  cols:  20,
-  rows:  14,
   moves: [STOP], 
   snake: [START],
   apple: state.apple,
@@ -327,8 +256,6 @@ const next = state => state.snake.length == 0
    : (state.apple.length == 0
       ? initialState()
       : ({
-      cols:  20,
-      rows:  14,
       moves: nextMoves(state),
       snake: nextSnake(state),
       apple: nextApple(state),
@@ -339,4 +266,4 @@ const next = state => state.snake.length == 0
 const enqueue = (state, move) => (state.moves.length < 4) ? merge(state)({ moves: state.moves.concat([move]) })
   : state
 
-module.exports = { EAST, NORTH, SOUTH, WEST, WALLS, initialState, enqueue, next, }
+module.exports = { COLS, ROWS, EAST, NORTH, SOUTH, WEST, WALLS, initialState, enqueue, next, }
