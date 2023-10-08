126e20f0040785e5bc03e8ac38e7371f08c6644d
Iury <136262198+ThiagoIury@users.noreply.github.com>
2023-10-08 13:43:37 -0300

    Update base.js

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 const dropFirst = xs => xs.slice(1) 
 // Combines two objects, replacing or adding values and properties of the second object to the first
-const merge     = o1 => o2 => Object.assign({}, o1, o2) 
+const merge = o1 => o2 => Object.assign({}, o1, o2) 
 // Random number within a range
 const randomNumber = min => max => Math.floor(Math.random() * max) + min
 const adjustInterval  = gameMapLimit => position => position < 0 
@@ -9,9 +9,9 @@ const adjustInterval  = gameMapLimit => position => position < 0
     ? 0 
     : position) 
 //Calculates the square of the distance between two given positions
-const distance  = (position1) =>  (position2) => Math.trunc((position2.x - position1.x) ** 2 + (position2.y - position1.y) ** 2)
+const distance  = position1 =>  position2 => Math.trunc((position2.x - position1.x) ** 2 + (position2.y - position1.y) ** 2)
 // Finds the smallest distance in relation to the birds
-const orderMoves = (moves) => (targetPosition) => (birdPosition) => {
+const orderMoves = moves => targetPosition => birdPosition => {
   
 // List of the distances
 const distances = moves.map(move => distance(targetPosition)({ x: birdPosition.x + move.x, y: birdPosition.y + move.y}))
