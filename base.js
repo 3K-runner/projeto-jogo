3c759b4b27f354d94a33fa431b00866f24278424
Iury <136262198+ThiagoIury@users.noreply.github.com>
2023-10-08 13:26:20 -0300

    Update base.js

--- base.js
+++ base.js
@@ -1,19 +1,25 @@
 const dropFirst = xs => xs.slice(1) 
+// Combines two objects, replacing or adding values and properties of the second object to the first
 const merge     = o1 => o2 => Object.assign({}, o1, o2) 
-const rnd       = min => max => Math.floor(Math.random() * max) + min
-const mod       = x => y => y < 0 ? (x - 1) : (y > (x - 1) ? 0 : y) 
-//Calculates the distance between two given dots
-const distance  = (p1) =>  (p2) => Math.trunc((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
+// Random number within a range
+const randomNumber = min => max => Math.floor(Math.random() * max) + min
+const adjustInterval  = gameMapLimit => position => position < 0 
+? (gameMapLimit - 1) 
+: (position > (gameMapLimit - 1) 
+    ? 0 
+    : position) 
+//Calculates the square of the distance between two given positions
+const distance  = (position1) =>  (position2) => Math.trunc((position2.x - position1.x) ** 2 + (position2.y - position1.y) ** 2)
 // Finds the smallest distance in relation to the birds
-const orderMoves = (moves) => (birdPosition) => (snakePosition) => {
+const orderMoves = (moves) => (targetPosition) => (birdPosition) => {
   
-  // Possible directions
-  const distances = moves.map((move) => distance(birdPosition)({ x: snakePosition.x + move.x, y: snakePosition.y + move.y}))
+// List of the distances
+const distances = moves.map(move => distance(targetPosition)({ x: birdPosition.x + move.x, y: birdPosition.y + move.y}))
 
-  // Ordered directions
- const movementInOrder = [...moves].sort((a, b) => distances[moves.indexOf(a)] - distances[moves.indexOf(b)])
+ // Order the moves by their distances 
+const movementInOrder = [...moves].sort((a, b) => distances[moves.indexOf(a)] - distances[moves.indexOf(b)])
 
   return movementInOrder;
 }
 
-module.exports = { dropFirst, merge, mod, rnd,distance,orderMoves}
+module.exports = { dropFirst, merge, adjustInterval, randomNumber, distance, orderMoves}
